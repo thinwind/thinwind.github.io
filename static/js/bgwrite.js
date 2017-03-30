20fb7d19be2dd5245170e67e1e84dd8bfe3137e6
Shang Yehua <niceshang@163.com>
2017-03-30 08:49:13 +0800

    添加背景js脚本

---
+++ static/js/bgwrite.js
@@ -0,0 +1,17 @@
+(function() {
+      var userAgentInfo = navigator.userAgent;
+      var Agents = ["Android", "iPhone",
+        "SymbianOS", "Windows Phone",
+        "iPad", "iPod"
+      ];
+      var flag = true;
+      for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+          flag = false;
+          break;
+        }
+      }
+      if(flag){
+        document.writeln("<script src='/static/js/canvas-nest.min.js'><\/script>");
+      }
+    })();
